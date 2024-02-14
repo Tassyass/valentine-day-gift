341ccb4f96f165966785cf9a2fa80beb24e02889
philipmuhoro <tasniim@DESKTOP-5THH7BR>
2024-02-14 12:13:11 +0300

    completed

---
+++ valentines-day-gift-planner/src/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      // Perform login logic here
+      // Redirect to gift list page or perform other actions after login
+      window.location.href = 'gift-list.html'; // Redirect to gift list page
+    });
+  });
+  
